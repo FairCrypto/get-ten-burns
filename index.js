f897da3a355867818c9c89e978a1f1de13749118
Leonid Belyaev <lbeliaev@gmail.com>
2024-09-26 09:20:44 -0700

    added XENFT query

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ const getBurnRate = (id) => {
   return burnRates[cls] || 0;
 }
 
+const decodeMintInfo = (mintInfo) => ({
+  term: parseInt(`0x${mintInfo.slice(0, 4)}`),
+  maturityTs: parseInt(`0x${mintInfo.slice(4, 20)}`),
+  rank: parseInt(`0x${mintInfo.slice(20, 52)}`),
+  amp: parseInt(`0x${mintInfo.slice(52, 56)}`),
+  eaa: parseInt(`0x${mintInfo.slice(56, 60)}`),
+  isApex: parseInt(`0x${mintInfo.slice(60, 62)}`) > 128,
+  isLimited: parseInt(`0x${mintInfo.slice(60, 62)}`) > 64 && parseInt(`0x${mintInfo.slice(60, 62)}`) < 128,
+  redeemed: parseInt(`0x${mintInfo.slice(62)}`) === 1
+});
+
 dotenv.config();
 
 async function main() {
@@ -44,37 +55,34 @@ async function main() {
   const { abi: xenCryptoAbi } = XenContract;
   const { abi: xenTorrentAbi } = XeNFTContract;
   const XEN = new Contract(contractAddress, xenCryptoAbi, provider);
+  const XENFT = new Contract(minterAddress, xenTorrentAbi, provider);
+
+  const getMintInfo = async (id) => {
+    const mintInfo = await XENFT.mintInfo(id).then(_ => BigInt(_.toString()));
+    return decodeMintInfo(mintInfo.toString(16).padStart(64, '0'));
+  }
+
+
   const burns = await XEN.userBurns(userAddress).then(_ => BigInt(_.toString()));
   const balance = await XEN.balanceOf(userAddress).then(_ => BigInt(_.toString()));
   console.log('Address:', userAddress)
   console.log('  XEN balance (wei):', balance)
   console.log('  XEN total burns (wei):', burns)
 
-  const XENFT = new Contract(minterAddress, xenTorrentAbi, provider);
-  // const events = await XENFT.queryFilter("*", '16300528', )
-
   const tokens = await XENFT.ownedTokens();
-  /*
-  const burnRates = []
-  for (const i of [0,1,2,3,4,5,6]) {
-    const specialClassesBurnRates = await XENFT.specialClassesBurnRates(i);
-    burnRates.push(specialClassesBurnRates.toString());
-  }
-  const classLimits = []
-  for (const i of [0,1,2,3,4,5,6]) {
-    const specialClassesTokenLimits = await XENFT.specialClassesTokenLimits(i);
-    classLimits.push(specialClassesTokenLimits.toString());
-  }
-  console.log('  XENFT burn rates', burnRates);
-  console.log('  XENFT limits', classLimits);
-   */
   console.log('  XENFT owned now', tokens.map(t => t.toString()));
+
   const minting = XENFT.filters.Transfer(ethers.constants.AddressZero, userAddress);
   const logs = await XENFT.queryFilter(minting, 16300528, "latest");
   console.log('  XENFT minted', logs.map(l => l.args?.tokenId.toString()));
-  console.log('  XENFT burns', logs.map(l => {
-    return `${l.args?.tokenId.toString()}: ${BigInt(getBurnRate(l.args?.tokenId.toString()))}`
-  }));
+
+  const burnRates = logs.map(l => getBurnRate(l.args?.tokenId.toString()));
+  const mintInfos = await Promise.all(logs.map(l => l.args?.tokenId.toString()).map(getMintInfo));
+  const merged = logs
+    .map((l, i) => ({ burnRate: burnRates[i], mintInfo: mintInfos[i]}))
+    .map(({ burnRate, mintInfo }) => ({ burnRate: mintInfo.isLimited? burnRates[1] : burnRate, mintInfo }));
+
+  console.log('  XENFT mint info', merged);
 }
 
 main().catch(console.error);
